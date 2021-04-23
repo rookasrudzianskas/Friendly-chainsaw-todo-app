9c5266fa0b15b2e9591396dafc47347f3843e847
Rokas <rookas.rudzenskas@gmail.com>
2021-04-23 10:40:07 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import './App.css';
+import React, {useState} from "react";
+import {Button} from "@material-ui/core";
+
+
 
 function App() {
+    const [todos, setTodos] = useState(['Take to shop', 'Take the rubbish out', 'Haha go to take bananas2']);
+    const [input, setInput] = useState('');
+    console.log('🤷‍', input);
+
+    const addTodo = (event) => {
+        // this will fire of then we click the button
+        event.preventDefault();
+        console.log('👽', 'It is working!')
+        setTodos([...todos, input]);
+        setInput(''); // clear up the form after the submit clicking add todo button!
+        console.log(todos);
+    }
+
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
-        <input type="text"/>
-        <button>Add Todo</button>
-
+      <h1>Hello world! 🚀</h1>
+        <form action="">
+        <input type="text" value={input} onChange={event => setInput(event.target.value)}/>
+        <Button disabled={!input} variant="contained" color="primary" type="submit" onClick={addTodo}>Add Todo</Button>
+        </form>
       <ul>
-        <li>Take dogs for a walk</li>
-        <li>Take the rubish out</li>
+          {todos.map(todo => (
+              <li>{todo}</li>
+          ))}
       </ul>
     </div>
   );
